89bb202ae843ba99014f029285dda0e21cb9b8f9
Hamaarour <amaarour.hicham97@gmail.com>
2024-04-06 05:45:41 +0000

    mariadb

--- scrs/requirements/mariadb/tools/db.sh
+++ scrs/requirements/mariadb/tools/db.sh
@@ -2,11 +2,8 @@
 
 
 sleep 5
-service mysql start 
+echo "CREATE DATABASE inception ;" > database.sql
+echo "GRANT ALL PRIVILEGES ON inception.* TO 'hicham'@'%' IDENTIFIED BY '123456';" >> database.sql
+echo "FLUSH PRIVILEGES;" >> database.sql
 
-
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS inception ;"
-mysql -u root -e "CREATE USER IF NOT EXISTS 'hicham'@'%' IDENTIFIED BY '123456789' ;" 
-mysql -u root -e "GRANT ALL PRIVILEGES ON inception.* TO 'hicham'@'%' ;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123456789' ;"
-mysql -u root -e "FLUSH PRIVILEGES ;"
+mysqld --user=mysql --init-file=/database.sql
