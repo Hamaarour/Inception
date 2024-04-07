49e51b4034fcb541aaddb7545fb9304f34758284
hamaarou <hamaarou@hamaarou.hamaarou>
2024-04-06 21:56:57 -0400

    mariadb service

--- scrs/requirements/mariadb/tools/db.sh
+++ scrs/requirements/mariadb/tools/db.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-
 sleep 5
-echo "CREATE DATABASE inception ;" > database.sql
-echo "GRANT ALL PRIVILEGES ON inception.* TO 'hicham'@'%' IDENTIFIED BY '123456';" >> database.sql
+echo "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" > database.sql
+echo "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" >> database.sql
+echo "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> database.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" >> database.sql
 echo "FLUSH PRIVILEGES;" >> database.sql
+
+service mariadb start
+mysqld < database.sql
+service mariadb stop
 
 mysqld --user=mysql --init-file=/database.sql
