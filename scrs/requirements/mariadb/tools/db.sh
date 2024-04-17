077fe23297a08d677b7daa11bc4f8f7c4d7f8546
hamaarou <hamaarou@hamaarou.hamaarou>
2024-04-17 15:29:27 +0100

    found bug in the mariadb script and FIX IT, DONE

--- scrs/requirements/mariadb/tools/db.sh
+++ scrs/requirements/mariadb/tools/db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sleep 10
-echo "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" > database.sql
-echo "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" >> database.sql
-echo "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> database.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" >> database.sql
-echo "FLUSH PRIVILEGES;" >> database.sql
-
 service mariadb start
-mysqld < database.sql
+
+sleep 2
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};\
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';\
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';\
+FLUSH PRIVILEGES;"
+
 service mariadb stop
 
-mysqld --user=mysql --init-file=/database.sql
+exec mysqld_safe --bind-address=$DB_HOST --port=3306
--- scrs/requirements/wordpress/tools/wp_script.sh
+++ scrs/requirements/wordpress/tools/wp_script.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-# until mysql -h"$DB_HOST" -u"$SQL_USER" -p"$SQL_PASSWORD" -e "USE $SQL_DATABASE" 2>/dev/null ; do
-#     echo "Wait for MariaDB to be ready!!"
-#     sleep 3
-# done
 
-# if ! [ -f wp-config.php ]; then
-#     wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$DB_HOST --allow-root
-#     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
-#     wp user create $WP_AUTHOR_LOGIN $WP_AUTHOR_EMAIL --role=subscriber --user_pass=$WP_AUTHOR_PASSWORD --allow-root
-# else
-#     echo "Wordpress is ready"
-# fi
-
-# exec php-fpm7.4 -F
 # Function to wait for MariaDB
-wait_for_mariadb() {
-  until mysql -h "$DB_HOST" -u "$SQL_USER" -p"$SQL_PASSWORD" -e "USE $SQL_DATABASE" &> /dev/null; do
+  until mariadb -h mariadb -u $SQL_USER -p$SQL_PASSWORD -e ";"
+  do
     echo "Waiting for MariaDB to be ready..."
     sleep 3
   done
-}
 
 # Initialize WordPress (if not already configured)
 initialize_wordpress() {
@@ -49,12 +35,11 @@ initialize_wordpress() {
       --role=subscriber \
       --user_pass="$WP_AUTHOR_PASSWORD" \
       --allow-root
+    wp theme install kubio --activate --allow-root
   else
     echo "WordPress is already configured."
   fi
 }
-
-wait_for_mariadb
 
 initialize_wordpress
 
